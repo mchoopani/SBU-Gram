4b9f226fef63fc70d3a72f92e79ff22bc538995c
mchoopani <choopani.m81@gmail.com>
2021-06-27 08:27:36 +0430

    additional files to better understand added.

---
+++ Javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"bridges"},{"l":"Controller"},{"l":"Model"},{"l":"server"},{"l":"server.DB"},{"l":"Widgets"}];updateSearchResults();
